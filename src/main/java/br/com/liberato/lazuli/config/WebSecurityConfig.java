7bab54ff4f1084f393b31307e86470a7ea8f0354
Elvis Liberato <liberatov13@gmail.com>
2022-04-19 00:34:08 -0300

    Utilizando webjars na tela de login

--- src/main/java/br/com/liberato/lazuli/config/WebSecurityConfig.java
+++ src/main/java/br/com/liberato/lazuli/config/WebSecurityConfig.java
@@ -37,6 +37,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/assets/**", "/css/**");
+        web.ignoring().antMatchers("/assets/**", "/css/**", "/webjars/**");
     }
 }
