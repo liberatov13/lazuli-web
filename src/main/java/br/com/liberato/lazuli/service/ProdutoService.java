a4c6a8eef52bcf5e88866048bbc2bcd8ed56ed4f
Elvis Liberato <liberatov13@gmail.com>
2022-05-02 00:44:52 -0300

    Tratando consulta de TipoProduto sem resultados

--- src/main/java/br/com/liberato/lazuli/service/ProdutoService.java
+++ src/main/java/br/com/liberato/lazuli/service/ProdutoService.java
@@ -7,6 +7,8 @@ import br.com.liberato.lazuli.repository.TipoProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -17,14 +19,22 @@ public class ProdutoService {
     @Autowired
     private TipoProdutoRepository tipoProdutoRepository;
 
-    public List<Produto> findProdutos(String tipoProuto) {
-        if (tipoProuto != null) {
-            TipoProuto tipo = tipoProdutoRepository.findByNome(tipoProuto).orElseThrow(() -> {
-                // TODO: Incluir tratamento para Tipos de Produto não localizados
-                throw new RuntimeException("Tipo de produto com descrição \"" + tipoProuto + "\" não encontrado.");
-            });
-            return produtoRepository.findProdutoByTipoProuto(tipo);
+    public List<Produto> findProdutos(String nomeTipoProduto) {
+        if (nomeTipoProduto != null) {
+            try {
+                return findProdutosByNomeTipoProduto(nomeTipoProduto);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+                return Collections.emptyList();
+            }
         }
         return produtoRepository.findAll();
+    }
+
+    private List<Produto> findProdutosByNomeTipoProduto(String nomeTipoProduto) throws EntityNotFoundException {
+        TipoProuto tipo = tipoProdutoRepository.findByNome(nomeTipoProduto)
+                .orElseThrow(() -> new EntityNotFoundException("Tipo de produto com descrição \"" + nomeTipoProduto + "\" não encontrado."));
+        return produtoRepository.findProdutoByTipoProuto(tipo);
     }
 }
